58aed052113532342aee816d2dcb3daee526afda
omeryildizce <omeryildizce@gmail.com>
2022-06-01 17:35:13 +0300

    Create c03_Num_boolean.dart
    Veri tipi kuralları yazıldı.

---
+++ bolum_01_Giris/c03_Num_boolean.dart
@@ -0,0 +1,33 @@
+void main(List<String> args) {
+  int yas = 34;
+  print(yas);
+
+  yas = 35;
+  print(yas);
+
+  num yil = 1998;
+  print(yil);
+
+  double sayi = 57;
+  print(sayi);
+
+  int kilo = 84.6.toInt();
+  print(kilo);
+
+  var sayi2 = 56;
+  print(sayi2);
+
+  int numara;
+  numara = 10;
+  print(numara);
+
+  double? s1 = null;
+  // null değer ataması yapılabilmesi için  değişkene ? eklenir
+  print(s1);
+  print(s1 = 5 + 0);
+
+  int hexaDecimalSayi = 0xAABBCC;
+  print(hexaDecimalSayi);
+
+  
+}
