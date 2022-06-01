dc4fbf957abbaf313dd226136f4eb4828e6d97a8
omeryildizce <omeryildizce@gmail.com>
2022-06-01 19:54:28 +0300

    Update c01_If_else.dart
    if, else ve if else işlemleri yapıldı.

--- bolum_02_If_else/c01_If_else.dart
+++ bolum_02_If_else/c01_If_else.dart
@@ -1,4 +1,36 @@
 void main(List<String> args) {
   int sayi1 = 12;
-  
+  num sayi2 = 12;
+  var sayi3 = 16;
+
+  if (sayi1 > sayi2) {
+    print("$sayi1 > $sayi2");
+  } else {
+    print("$sayi1 < $sayi2");
+  }
+
+  if (sayi1 < sayi2) {
+    print("$sayi1 < $sayi2");
+  } else if (sayi2 < sayi1) {
+    print("$sayi2 < $sayi1");
+  } else {
+    print("$sayi1 = $sayi2");
+  }
+
+  print("***********************************");
+
+  int not = 90;
+  if (not >= 0 && not <= 100) {
+    if (not < 60) {
+      print("F");
+    } else if (not < 75) {
+      print("C");
+    } else if (not < 85) {
+      print("B");
+    } else {
+      print("A");
+    }
+  } else {
+    print("Hatalı not değeri!");
+  }
 }
