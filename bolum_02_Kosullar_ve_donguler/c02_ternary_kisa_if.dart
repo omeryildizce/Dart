f3b4b855dc2a7ff0cb6b7dceeda7b1811ee1c5c0
omeryildizce <omeryildizce@gmail.com>
2022-06-01 19:59:52 +0300

    Update
    Klasör ismi güncellendi.

---
+++ bolum_02_Kosullar_ve_donguler/c02_ternary_kisa_if.dart
@@ -0,0 +1,10 @@
+/*
+koşul ? dogru ise : yanlış ise
+
+null değerlerin kontrolünde ?? kulanılır.
+koşul1 ?? koşul2
+koşul1 null değise kullanılır, eğer null ise koşul2 kullanılır.
+*/ 
+void main(List<String> args) {
+  
+}
