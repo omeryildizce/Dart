b4e2a97d28681e7c714281e0b23a510fd4365879
omeryildizce <omeryildizce@gmail.com>
2022-06-01 17:20:04 +0300

    Create c02_VeriTipleri_Degiskenler.dart
    Veri tipleri ve değişkenler ile ilgili uygulama yazıldı.

---
+++ bolum_01_Giris/c02_VeriTipleri_Degiskenler.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  int yas = 34;
+  double ortalama = 5.6;
+  num yil = 1998;
+
+  print(yas);
+  print(ortalama);
+  print(yil);
+
+  String ad = "Ömer";
+  print(ad);
+
+  ad = "Semih";
+  print(ad + "in sevdiği renkler.");
+  print(ad + "adin yaşı");
+
+  bool dogru = true;
+  print(dogru);
+
+  var okul = "Ege";
+  var number = 14;
+
+  print(okul);
+  print(number);
+ }
