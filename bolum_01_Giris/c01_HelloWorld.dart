ca0f068e0075420c497a07eaec5ac3c4dd5f9541
omeryildizce <omeryildizce@gmail.com>
2022-06-01 16:55:28 +0300

    Create c01_HelloWorld.dart
    İlk dart uygulaması yazıldı.

---
+++ bolum_01_Giris/c01_HelloWorld.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print("Hello World");
+  print("Ömer Yıldız");
+  print("3+5");
+  print(3 + 5);
+  print("Semih'in dersleri");
+  print("Semih" + "Şahin");
+
+  // ilk dart uygulaması yazılır
+}
